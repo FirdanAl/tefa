b7995073415de669bb763c032bc078734521b3a2
FirdanAl <144082357+FirdanAl@users.noreply.github.com>
2024-05-03 10:22:55 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="row p-3 bg-primary text-white">
+        <div class="col-lg-1">
+            <img src="../assets/img/LOGO-SMK4.png" class="LOGO">
+        </div>
+        <div class="col-lg-11 kj">
+            <h1>Perpus Digital</h1>
+            <address>
+                SMK NEGERI 4 KOTA TASIKMALAYA<br>
+                Jl.Depok, Kel.Sukamenak, Kec.Purbaratu.
+            </address>
+        </div>
+    </div>
+</template>
---
+++ components/Statistik.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <canvas id="statistik"></canvas>
+    </div>
+</template>
+
+<script setup>
+import Chart from 'char.js/auto'
+
+const labels = [
+    'Januari',
+    'Pebruari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember',
+];
+
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'Pengujung',
+        data: [1, 5, 7, 4, 10, 6, 8, 9, 0, 2,],
+        backgroundColor: [
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+            'rgb(0, 120, 255)',
+        ]
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+};
+
+onMounted(() => {
+    const myChart = new Chart(
+        document.getElementById('statistik'),
+    );
+})
+</script>
